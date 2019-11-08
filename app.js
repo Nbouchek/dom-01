4f2d34593de445ba289c1ef0a2384b99eba071ef
Nacer Bouchekhima <nbouchek@gmail.com>
2019-11-08 12:22:39 +0000

    change list item - demo

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ h1.textContent = 'Nacer';
 h1.style.color = 'black';
 h1.style.backgroundColor = 'green';
 
+const li = document.querySelector('li:last-of-type');
+li.textContent = li.textContent = ' (changed!)';
+
 const allLis = document.querySelectorAll('li');
 const allLisByTagName = document.getElementsByTagName('li');
 
