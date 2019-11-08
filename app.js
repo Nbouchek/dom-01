86bcaeff2c797f7d9baed6813be273d2dfc5e7be
Nacer Bouchekhima <nbouchek@gmail.com>
2019-11-08 12:20:19 +0000

    change properties using js

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+const h1 = document.getElementById('main-title');
+h1.textContent = 'Nacer';
+
+h1.style.color = 'black';
+h1.style.backgroundColor = 'green';
+
 const allLis = document.querySelectorAll('li');
 const allLisByTagName = document.getElementsByTagName('li');
 
